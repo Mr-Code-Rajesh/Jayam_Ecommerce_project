dbad24b3ccb5d3a22e21932753c0973438040838
Rajesh P <n-rajesh.p@draup.com>
2025-11-01 19:22:28 +0530

    server.js updated

--- api-server/server.js
+++ api-server/server.js
@@ -1,23 +1,22 @@
+// api-server/server.js
 import jsonServer from "json-server";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-server.use(cors());
-server.use(middlewares);
+server.use(cors({ origin: process.env.CORS_ORIGIN || "*" }));
 server.use(jsonServer.bodyParser);
+server.use(middlewares);
 
 server.get("/", (req, res) => {
-  res.send(" JSON Server is running!");
+  res.json({ message: " JSON Server is running successfully on Vercel!" });
 });
 
 server.use("/api", router);
 
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`JSON Server running on port ${PORT}`);
-});
-
+export default server;
--- ecommerce/src/Utilize/Firebase.js
+++ ecommerce/src/Utilize/Firebase.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBK86Y-s82IVZGSlGSIFM_wm8faGObhpwk",
-  authDomain: "my-ecommerce-app-ac3cf.firebaseapp.com",
-  projectId: "my-ecommerce-app-ac3cf",
-  storageBucket: "my-ecommerce-app-ac3cf.firebasestorage.app",
-  messagingSenderId: "799406371755",
-  appId: "1:799406371755:web:51d7f13a365beb00c858ee",
-  measurementId: "G-ZQBBKYYDJG"
-};
+  const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
